d924bdc1ce87a8519935c163020e31cf14701a98
yqm-307 <979336542@qq.com>
2023-07-21 14:28:49 +0000

    alloc文件

---
+++ project/server/util/alloc/myalloc.cc
@@ -0,0 +1,13 @@
+#include "myalloc.hpp"
+#include <malloc.h>
+using namespace game::server::util::alloc;
+
+void* G_Hook_Alloc(size_t nbytes)
+{
+    return malloc(nbytes);
+}
+
+void G_Hook_Free(void* ptr)
+{
+    free(ptr);
+}
