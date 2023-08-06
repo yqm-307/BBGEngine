a89cb1147da4aae47b301e50c192120dc774c6b8
yqm-307 <979336542@qq.com>
2023-08-06 23:27:35 +0800

    删除接口

--- project/server/util/lockfree/LockFreeQueueTpl.hpp
+++ project/server/util/lockfree/LockFreeQueueTpl.hpp
@@ -14,34 +14,41 @@ Queue<T, size>::Queue()
 template <typename T, size_t size> 
 bool Queue<T, size>::Push(const T &element) 
 {
-    size_t w_count = m_write_count.load(std::memory_order_relaxed);
+    /* 已经写了多少个了 */
+    size_t current_count = m_write_count.load(std::memory_order_relaxed);
 
     while (true) {
-        const size_t index = w_count % size;
+        /* 计算循环数组的下标 */
+        const size_t index = current_count % size;
+        /* 获取该槽的push计数，后面对这个值还有修改，需要保证不会吧重排 */
+        const size_t slot_push_count = m_data[index].push_count.load(std::memory_order_acquire);
+        /* 获取该槽的pop计数，在push操作中，只读所以保证原子性即可 */
+        const size_t slot_pop_count = m_data[index].pop_count.load(std::memory_order_relaxed);
 
-        const size_t push_count =
-            m_data[index].push_count.load(std::memory_order_acquire);
-        const size_t pop_count =
-            m_data[index].pop_count.load(std::memory_order_relaxed);
-
-        if (push_count > pop_count) {
+        /* 首次确认当前槽的插入合法性，对于每个槽，push操作一定小于等于pop操作次数 */
+        if (slot_push_count > slot_pop_count) 
             return false;
-        }
 
-        const size_t revolution_count = w_count / size;
-        const bool our_turn = revolution_count == push_count;
+        /* 已经循环圈数 */ 
+        const size_t revolution_count = current_count / size;
+        /* 再次确认这个槽的插入合法性 */ 
+        const bool our_turn = revolution_count == slot_push_count;
 
-        if (our_turn) {
-            /* Try to acquire the slot by bumping the monotonic write counter */
-            if (m_write_count.compare_exchange_weak(w_count, w_count + 1U,
-                                               std::memory_order_relaxed)) {
+        if (our_turn) 
+        {
+            /* 通过竞争获得写入的权限，保证原子性即可 */
+            if (m_write_count.compare_exchange_weak(current_count, current_count + 1U, std::memory_order_relaxed)) 
+            {
+                /* 内部是安全的操作，因为获得了第 m_write_count 位的写入权限，其他人需要竞争后面的位置 */
                 m_data[index].val = element;
-                m_data[index].push_count.store(push_count + 1U,
-                                              std::memory_order_release);
+                /* 使用 memory_order_release 是因为，作为最终的标志结束的标志位。不允许重排到前面 */
+                m_data[index].push_count.store(slot_push_count + 1U, std::memory_order_release);
                 return true;
             }
-        } else {
-            w_count = m_write_count.load(std::memory_order_relaxed);
+        } 
+        else 
+        {
+            current_count = m_write_count.load(std::memory_order_relaxed);
         }
     }
 }
@@ -49,48 +56,34 @@ bool Queue<T, size>::Push(const T &element)
 template <typename T, size_t size> 
 bool Queue<T, size>::Pop(T &element) 
 {
+    /* 读保证原子性即可 */
     size_t r_count = m_read_count.load(std::memory_order_relaxed);
 
-    while (true) {
+    while (true) 
+    {
         const size_t index = r_count % size;
+        /* 有修改，保证可见性，防止重排 */
+        const size_t pop_count = m_data[index].pop_count.load(std::memory_order_acquire);
+        const size_t push_count = m_data[index].push_count.load(std::memory_order_relaxed);
 
-        const size_t pop_count =
-            m_data[index].pop_count.load(std::memory_order_acquire);
-        const size_t push_count =
-            m_data[index].push_count.load(std::memory_order_relaxed);
-
-        if (pop_count == push_count) {
+        if (pop_count == push_count) 
             return false;
-        }
 
         const size_t revolution_count = r_count / size;
         const bool our_turn = revolution_count == pop_count;
 
-        if (our_turn) {
-            /* Try to acquire the slot by bumping the monotonic read counter. */
-            if (m_read_count.compare_exchange_weak(r_count, r_count + 1U,
-                                               std::memory_order_relaxed)) {
+        if (our_turn) 
+        {
+            /* 通过竞争获得读取的权限，保证原子性即可 */
+            if (m_read_count.compare_exchange_weak(r_count, r_count + 1U, std::memory_order_relaxed)) 
+            {
                 element = m_data[index].val;
-                m_data[index].pop_count.store(pop_count + 1U,
-                                             std::memory_order_release);
+                m_data[index].pop_count.store(pop_count + 1U, std::memory_order_release);
                 return true;
             }
-        } else {
+        } 
+        else 
             r_count = m_read_count.load(std::memory_order_relaxed);
-        }
-    }
-}
-
-template <typename T, size_t size>
-std::optional<T> Queue<T, size>::PopOptional() 
-{
-    T element;
-    bool result = Pop(element);
-
-    if (result) {
-        return element;
-    } else {
-        return {};
     }
 }
 
