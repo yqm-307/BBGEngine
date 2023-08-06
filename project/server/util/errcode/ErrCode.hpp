dcea43ce31af27ebf23d4d9893ce1729b7c740c7
yqm-307 <979336542@qq.com>
2023-08-06 23:27:12 +0800

    新的工具类-errcode，统一错误码

---
+++ project/server/util/errcode/ErrCode.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include <cstdarg>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include "util/errcode/ErrCodeDef.hpp"
+
+namespace game::util::errcode
+{
+
+
+class ErrCode
+{
+public:
+    explicit ErrCode(const std::string& err_info,ErrType errtype,int errcode);
+
+    ErrCode();
+    void SetInfo(const std::string& errinfo);
+
+    void SetInfo(const char* fmt, ...);
+
+
+    void SetCode(int code);
+
+
+    void SetType(ErrType type);
+
+
+    void Set(const std::string& err_info,ErrType errtype,int errcode);
+
+
+    const std::string& What() const;
+
+    int Err() const;
+
+
+private:
+    std::string     m_err_info;
+    ErrType         m_err_type;
+    int             m_errcode;
+
+};
+
+}// namespace end
