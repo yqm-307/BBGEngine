d0c0e272d4f4b5114e6debfbe1287874ccb4f049
yqm-307 <979336542@qq.com>
2023-08-17 09:20:10 +0000

    主场景主线程添加信号处理机制（SIGINT）

--- project/server/gameserver/scene/GameServerScene.cc
+++ project/server/gameserver/scene/GameServerScene.cc
@@ -1,5 +1,6 @@
 #include "gameserver/scene/GameServerScene.hpp"
 #include "util/log/Log.hpp"
+#include <signal.h>
 
 namespace server::scene
 {
@@ -20,13 +21,25 @@ void GameServerScene::OnUpdate()
     module_aoi->OnUpdate();  // 更新aoi
 }
 
-#pragma region "内部函数"
+#pragma region "事件函数"
 
 void EventUpdate(evutil_socket_t,short,void* arg)
 {
     auto pthis = (GameServerScene*)arg;
     pthis->OnUpdate();
 }
+
+void EventSignal_Sigint(evutil_socket_t, short, void* arg)
+{
+    auto pthis = (GameServerScene*)arg;
+    pthis->StopScene();
+}
+
+#pragma endregion
+
+#pragma region "内部函数"
+
+
 
 void GameServerScene::Init()
 {
@@ -37,18 +50,21 @@ void GameServerScene::Init()
     [](GameObjectSPtr, GameObjectSPtr){
     });
 
+    m_ev_base = event_base_new();
     {// scene update 事件注册
-        m_ev_base = event_base_new();
         timeval tv;
         evutil_timerclear(&tv);
         tv.tv_usec = GameSceneFrame * 1000;
         /* 随意的暴露this，这不是一个很好的做法。todo：寻求更好的兼容C风格Api的方式 */
-        event* m_update_event = event_new(m_ev_base, -1, EV_PERSIST, EventUpdate, this);
+        m_update_event = event_new(m_ev_base, -1, EV_PERSIST, EventUpdate, this);
+        Assert(m_update_event != nullptr);
         Assert(event_add(m_update_event, &tv) == 0);
     }
 
     {// SIGINT 信号事件注册
-        
+        m_signal_sigint = evsignal_new(m_ev_base, SIGINT, EventSignal_Sigint, this);
+        Assert(m_signal_sigint != nullptr);
+        Assert(event_add(m_signal_sigint, NULL) == 0);
     }
 }
 
@@ -58,9 +74,13 @@ void GameServerScene::Destory()
     module_aoi = nullptr;
 
     event_free(m_update_event);
+    event_free(m_signal_sigint);
     event_base_free(m_ev_base);
+
     m_ev_base = nullptr;
     m_update_event = nullptr;
+    m_signal_sigint = nullptr;
+
 }
 
 void GameServerScene::StartScene()
@@ -71,6 +91,7 @@ void GameServerScene::StartScene()
 void GameServerScene::StopScene()
 {
     event_base_loopbreak(m_ev_base);
+    BBT_BASE_LOG_INFO("SIGINT exist loop!");
 }
 
 #pragma endregion
