dcc5d5428b5a8194d7fc46a30fba5322036d4700
yqm-307 <979336542@qq.com>
2023-09-03 23:49:30 +0800

    在ConnMgr中添加心跳检测，并添加心跳相关的配置

--- project/server/util/network/libevent/evConnMgr.hpp
+++ project/server/util/network/libevent/evConnMgr.hpp
@@ -2,6 +2,7 @@
 #include "util/network/ConnMgr.hpp"
 #include "util/network/libevent/evConnection.hpp"
 #include <map>
+#include <event2/event.h>
 
 namespace game::util::network::ev
 {
@@ -25,18 +26,40 @@ public:
      * @param local_ip 本地的ip、port
      * @return ConnectionSPtr 
      */
-    virtual ConnectionSPtr CreateConn(IOThread* thread, int newfd, Address peer_ip, Address local_ip);
+    [[nodiscard("创建后需要保存新连接")]] virtual ConnectionSPtr CreateConn(IOThread* thread, int newfd, Address peer_ip, Address local_ip);
     /*XXX 通过socketfd获取连接，寻求更好的方式，暴露出socket fd可能是不好的，可以加入句柄/id */
     virtual ConnectionWKPtr GetConnBySocket(int sockfd); 
+
+    /* 设置IO Thread */
+    void SetIOThread(IOThread*);
 private:
     evConnMgr();
     virtual ~evConnMgr();
 
+    void InitCfg();
+    void InitEvent();
+
     void OnConnInit(evConnectionSPtr conn);
     void OnConnDestory(evConnectionSPtr conn);
 private:
+    /* 心跳事件控制 */
+    void InitHeartBeatEvent();
+    void DestoryHeartBeatEvent();
+private:
+    /** 心跳检测：遍历并检测所有连接事件，对超时的连接事件进行通知
+     * 在acceptor线程里面 event_base 里面注册这个事件。定时调用，以
+     * 实现定时检测心跳。在acceptor线程的原因是它的负载低
+     */
+    void OnHeartBeatOnce();
+private:
     std::map<evutil_socket_t, ConnectionSPtr> m_conn_map;
     std::mutex  m_conn_map_mutex;
+    IOThread*   m_thread{nullptr};
+    /* 心跳超时的时间间隔 */
+    bbt::timer::clock::ms m_heartbeat_timeout_duration;
+    /* 心跳检查的时间间隔 */
+    int         m_check_heartbeat_interval_ms;
+    event*      m_heartbeat_check_event;
 };
 
 }// namespace end
