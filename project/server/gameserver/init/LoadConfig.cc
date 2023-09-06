1ebce09727d9c2cbdd069c7fe90ddec6cafb1735
yqm-307 <979336542@qq.com>
2023-09-06 13:32:28 +0000

    添加修改配置

--- project/server/gameserver/init/LoadConfig.cc
+++ project/server/gameserver/init/LoadConfig.cc
@@ -24,6 +24,12 @@ void ServerConfig::Init()
     AssertWithInfo(m_config.server_ip != "", "fatal: server ip is null!");
     m_config.server_port = m_ini_reader->GetInteger("server", "port", 0);
     AssertWithInfo(m_config.server_port > 0, "fatal: server port is bad!");
+
+    m_config.m_connmgr_cfg.m_heart_beat_once_ms = m_ini_reader->GetInteger("heartbeat", "interval", 0);
+    AssertWithInfo(m_config.m_connmgr_cfg.m_heart_beat_once_ms > 0, "fatal: heartbeat is invalid!");
+    m_config.m_connmgr_cfg.m_heart_beat_timeout_ms = m_ini_reader->GetInteger("heartbeat", "checkonce", 0);
+    AssertWithInfo(m_config.m_connmgr_cfg.m_heart_beat_timeout_ms > 0, "fatal: heartbeat check once invalid!");
+    G_SetConfigPtr(game::util::config::evConnMgrConfig, &m_config.m_connmgr_cfg, game::util::config::Cfg_evConnMgr);
 }
 
 
